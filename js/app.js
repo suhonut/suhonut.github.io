f640c29b91f3a6d73f8a35f0312dbecabf669d79
suho-papa <boolancpain@gmail.com>
2024-11-14 17:45:26 +0900

    init

---
+++ js/app.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // naver map
+    loadMap();
+
+    var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+});
+
+const loadMap = () => {
+    const mapOpts = {
+        center: new naver.maps.LatLng(37.595732, 127.172496),
+        zoom: 16,
+    }
+
+    // map
+    const map = new naver.maps.Map(document.getElementById('map'), mapOpts);
+
+    var marker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(37.595732, 127.172496),
+        map: map,
+        animation: naver.maps.Animation.BOUNCE
+    });
+}
