eee18cb5ebd9b3375aa408f1ea9f9b91e2255014
suho-papa <boolancpain@gmail.com>
2024-12-16 14:34:53 +0900

    add gallery, profile images

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // gallery
+    loadGallery();
+
     // naver map
     loadMap();
 
@@ -19,6 +22,22 @@ document.addEventListener('DOMContentLoaded', function() {
         },
     });
 });
+
+const loadGallery = () => {
+    const galleryBody = document.getElementById('gallery-body');
+    // 1-20
+    const numbers = Array.from({ length: 20 }, (_, i) => i + 1);
+    numbers.forEach(num => {
+        const swiperSlide = document.createElement('div');
+        swiperSlide.className = 'swiper-slide';
+
+        const swiperImage = document.createElement('img');
+        swiperImage.src = `images/g${num}.png`;
+
+        swiperSlide.appendChild(swiperImage);
+        galleryBody.appendChild(swiperSlide);
+    });
+}
 
 const loadMap = () => {
     const mapOpts = {
