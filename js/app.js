53fa0b7116ca5ec3f6c1a80014ba7ba689a5580b
suho-papa <boolancpain@gmail.com>
2025-02-04 17:31:56 +0900

    add overlay

--- js/app.js
+++ js/app.js
@@ -1,35 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var swiper = new Swiper(".mySwiper", {
-        effect: "coverflow",
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: "auto",
-        zoom: true,
-        coverflowEffect: {
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-        },
-    });
+	var swiper = new Swiper(".mySwiper", {
+		effect: "coverflow",
+		grabCursor: true,
+		centeredSlides: true,
+		slidesPerView: "auto",
+		coverflowEffect: {
+			rotate: 50,
+			stretch: 0,
+			depth: 100,
+			modifier: 1,
+			slideShadows: true,
+		},
+		pagination: {
+			el: ".swiper-pagination",
+		},
+	});
+
+	// LightGallery 초기화
+	lightGallery(document.getElementById('lightgallery'), {
+		selector: 'a',
+		thumbnail: true,
+		download: false
+	});
 });
 
 const loadMap = () => {
-    const mapOpts = {
-        center: new naver.maps.LatLng(37.595732, 127.172496),
-        zoom: 16,
-    }
+	const mapOpts = {
+		center: new naver.maps.LatLng(37.595732, 127.172496),
+		zoom: 16,
+	}
 
-    // map
-    const map = new naver.maps.Map(document.getElementById('map'), mapOpts);
+	// map
+	const map = new naver.maps.Map(document.getElementById('map'), mapOpts);
 
-    var marker = new naver.maps.Marker({
-        position: new naver.maps.LatLng(37.595732, 127.172496),
-        map: map,
-        animation: naver.maps.Animation.BOUNCE
-    });
+	var marker = new naver.maps.Marker({
+		position: new naver.maps.LatLng(37.595732, 127.172496),
+		map: map,
+		animation: naver.maps.Animation.BOUNCE
+	});
 }
